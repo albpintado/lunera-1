b9c846845fc8d72da19bdc81cef7412680264ef1
Pintado <alberto.pintado@solera.com>
2022-11-09 13:51:24 +0100

    Add styles

--- src/components/Team.js
+++ src/components/Team.js
@@ -1,12 +1,13 @@
 import React from "react";
-
+import "./Team.css";
 const Team = ({ name, score }) => {
   return (
-    <article className="box">
-      <header>
-        <h3>{name}</h3>
-      </header>
-      <p>{score}</p>
+    <article className="box card card-body">
+      <h5 className="card-title">{name}</h5>
+      <p className="card-text">{score}</p>
+      <a href="#" className="card-link">
+        TASKS
+      </a>
     </article>
   );
 };
